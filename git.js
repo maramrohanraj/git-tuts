ce65a30410af2f568495e38527a4ee428e622891
Maram Rohan Raj <maramrohanraj@gmail.com>
2024-07-25 23:46:59 +0530

    commiting code

---
+++ git.js
@@ -0,0 +1,60 @@
+/* 
+git & github part-1 -> vcs and git repors
+part-2 -> staging area and commits
+part-3 -> working with branches
+part-4 -> issues and pull requests
+
+*/
+
+//* version control system(vcs)
+/* 
+git  means version constrol system
+if we are developing a website which have traffic and regularly we are adding updates and again deploy it 
+in software development bugs will come when updates come so we want to revert back 
+manaki new version lo bugs vunte patha version ni save cheyyali 
+to save the project we use vcs using remote server and track the changes 
+
+*/
+
+//*git concepts
+
+/* 
+we will have something remote repository (repository means a folder for project)
+1)remote repository
+2) local repository 
+3) staging area
+4) working directory
+
+
+*working directory
+for working in git we should use command
+1)git init
+2)git commit
+3) git status -> to check the status
+appude create chesi git command emi run cheyyaledhu dhanne working directory antamu
+untracked section is called working directory
+a.js,git.js,index.html are there in working directory we want to save it for future
+ 
+*/
+//* from working directory to staging area we will send one by one files 
+/* 
+working->staging area --> git add
+staging area-> local repo  ---> git commit
+local repo -> remote repo ==> git push
+
+*/
+
+
+
+//*staging area
+/* 
+ it is in between the local repository and remote repository
+ if we use staging area then we can commit what we need remaining will not be committed
+
+*/
+
+//*github
+
+/* 
+one example of storing remote repository hosting service
+*/
